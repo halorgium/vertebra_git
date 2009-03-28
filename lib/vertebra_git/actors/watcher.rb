e8b1376f5671487bb971240d120b4d88f811e377
Tim Carey-Smith <tim@spork.in>
2009-03-28 23:31:24 +1300

    First commit

---
+++ lib/vertebra_git/actors/watcher.rb
@@ -0,0 +1,27 @@
+module VertebraGit
+  module Actors
+    class Watcher < Vertebra::Actor
+      provides "/git"
+
+      bind_op "/code/commit", :code_commit
+      desc "/code/commit", "Announce a commit"
+      def code_commit(operation, args)
+        repository = args['repository']
+        commit = args["commit"]
+        puts "Got a commit for #{repository}"
+        pp commit
+
+        topic = commit['message'].split("\n").first
+        ref =   commit['id'][0,7]
+        author = commit['author']['name']
+        message = "[#{repository}] #{topic} - #{ref} - #{author}"
+
+        puts message
+
+        args = {:channel => "#halorgium", :message => message)
+        @agent.request("/irc/push", :single, args)
+        true
+      end
+    end
+  end
+end
